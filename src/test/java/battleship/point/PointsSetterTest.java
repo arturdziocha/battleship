692a08fa256e3661c41e9bbfd4716fabddf1d157
arturdziocha <artur.dziocha@gmail.com>
2019-10-16 16:49:42 +0200

    ss

--- src/test/java/battleship/point/PointsSetterTest.java
+++ src/test/java/battleship/point/PointsSetterTest.java
@@ -1,7 +1,21 @@
 package battleship.point;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+import org.junit.jupiter.api.Test;
+
+import battleship.direction.Direction;
+import battleship.exception.DirectionException;
+import battleship.exception.MalformedException;
+import battleship.exception.PointException;
 
 class PointsSetterTest {
+    @Test
+    void shouldCreate3PointsUp() throws MalformedException, DirectionException, PointException {
+        Point[] points = {new Point.Builder("B2").build(), new Point.Builder("b3").build(), new Point.Builder(3,1).build()};
+        PointsSetter pointSetter = new PointsSetter.Builder(3).startPoint(new Point.Builder("b2").build()).direction(Direction.DOWN).build();
+        assertThat(pointSetter.getPoints(), containsInAnyOrder(points));
+    }
 
 }
