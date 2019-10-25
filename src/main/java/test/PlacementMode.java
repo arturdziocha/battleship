43572246862237543abc039b54ccd28e707f6e11
arturdziocha <artur.dziocha@gmail.com>
2019-10-25 23:18:54 +0200

    s

---
+++ src/main/java/test/PlacementMode.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.Scanner;
+
+public class PlacementMode {
+	public static void main(String[] args) {
+		System.out.println("Placement mode");
+		System.out.println(">>>");
+		Scanner reader = new Scanner(System.in);
+		int placementMode = reader.nextInt();
+		
+		boolean placementModeSuccess = false;
+        while (placementModeSuccess == false) {   
+        	
+        }
+		
+		
+	}
+}
