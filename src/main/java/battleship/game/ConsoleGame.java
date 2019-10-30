17b60102f447d49ade5764bc6bdb5cb384127667
arturdziocha <artur.dziocha@gmail.com>
2019-10-30 22:01:18 +0100

    Working now

--- src/main/java/battleship/game/ConsoleGame.java
+++ src/main/java/battleship/game/ConsoleGame.java
@@ -23,295 +23,285 @@ import battleship.view.ConsoleView;
 import battleship.view.View;
 
 public class ConsoleGame implements Game {
-    private Player firstPlayer, secondPlayer, currentPlayer;
-    private Scanner reader = new Scanner(System.in);
-    private final View view;
+	private Player firstPlayer, secondPlayer, currentPlayer;
+	private Scanner reader = new Scanner(System.in);
+	private final View view;
 
-    public ConsoleGame(View view) {
-        this.view = view;
-    }
+	public ConsoleGame(View view) {
+		this.view = view;
+	}
 
-    @Override
-    public void play() {
-        view.welcomeUsers();
-        view.showInstructions();
-        int gameMode = setupGameMode();
-        setupPlayers(gameMode);
-        currentPlayer = firstPlayer;
-        if (isReadyToStart()) {
-            while (isGameFinished() == false) {
-                receiveShot();
-            }
-        }
-    }
+	@Override
+	public void play() {
+		view.welcomeUsers();
+		view.showInstructions();
+		int gameMode = setupGameMode();
+		setupPlayers(gameMode);
+		currentPlayer = firstPlayer;
+		if (isReadyToStart()) {
+			while (isGameFinished() == false) {
+				receiveShot();
+			}
 
-    private int setupGameMode() {
-        boolean gameModeChecker = false;
-        int gameMode = -1;
-        while (gameModeChecker == false) {
-            view.showGameMode();
-            if (reader.hasNextInt()) {
-                gameMode = reader.nextInt();
-                if (gameMode == 0 || gameMode == 1 || gameMode == 2) {
-                    gameModeChecker = true;
-                } else {
-                    System.out.printf(ConsoleView.error, "Please select valid Game Mode");
-                    gameModeChecker = false;
-                }
-            } else {
-                System.out.printf(ConsoleView.error, "Enter valid integer value!\n");
-                reader.next();
-            }
-        }
-        return gameMode;
-    }
+		}
+	}
 
-    private void setupPlayers(int setupMode) {
+	private int setupGameMode() {
+		boolean gameModeChecker = false;
+		int gameMode = -1;
+		while (gameModeChecker == false) {
+			view.showGameMode();
+			if (reader.hasNextInt()) {
+				gameMode = reader.nextInt();
+				if (gameMode == 0 || gameMode == 1 || gameMode == 2) {
+					gameModeChecker = true;
+				} else {
+					System.out.printf(ConsoleView.error, "Please select valid Game Mode");
+					gameModeChecker = false;
+				}
+			} else {
+				System.out.printf(ConsoleView.error, "Enter valid integer value!\n");
+				reader.next();
+			}
+		}
+		return gameMode;
+	}
 
-        switch (setupMode) {
-        case 0:
-            setupHumanPlayers();
-            break;
-        case 1:
-        case 2:
-            setupHumanWithComputer(setupMode);
-            break;
+	private void setupPlayers(int setupMode) {
 
-        }
-    }
+		switch (setupMode) {
+		case 0:
+			setupHumanPlayers();
+			break;
+		case 1:
+		case 2:
+			setupHumanWithComputer(setupMode);
+			break;
 
-    private void setupHumanPlayers() {
-        firstPlayer = setupPlayer(1);
-        secondPlayer = setupPlayer(2);
+		}
+	}
 
-    }
+	private void setupHumanPlayers() {
+		firstPlayer = setupPlayer(1);
+		secondPlayer = setupPlayer(2);
 
-    private void setupHumanWithComputer(int setupMode) {
-        firstPlayer = setupPlayer(1);
-        Fleet secondPlayerFleet = setupRandomShips(new FleetImpl.Builder().build());
-        secondPlayer = setupMode == 1 ? new EasyComputerPlayer.Builder(secondPlayerFleet).build()
-                : new HardComputerPlayer.Builder(secondPlayerFleet).build();
+	}
 
-    }
+	private void setupHumanWithComputer(int setupMode) {
+		firstPlayer = setupPlayer(1);
+		Fleet secondPlayerFleet = setupRandomShips(new FleetImpl.Builder().build());
+		secondPlayer = setupMode == 1 ? new EasyComputerPlayer.Builder(secondPlayerFleet).build()
+		        : new HardComputerPlayer.Builder(secondPlayerFleet).build();
 
-    private Player setupPlayer(int playerId) {
-        String name = setupName(playerId);
-        Fleet fleet = setupFleet(name);
+	}
 
-        return new ConsolePlayer.Builder(name, fleet).build();
-    }
+	private Player setupPlayer(int playerId) {
+		String name = setupName(playerId);
+		Fleet fleet = setupFleet(name);
 
-    private String setupName(int playerId) {
-        view.showNameSelect(playerId);
-        return reader.next();
-    }
+		return new ConsolePlayer.Builder(name, fleet).build();
+	}
 
-    private Fleet setupFleet(String playerName) {
-        Fleet fleet = new FleetImpl.Builder().build();
-        int placementMode = setupPlacementMode(playerName);
+	private String setupName(int playerId) {
+		view.showNameSelect(playerId);
+		return reader.next();
+	}
 
-        if (placementMode == 1) {
-            fleet = setupRandomShips(fleet);
-        } else if (placementMode == 0) {
-            fleet = setupConsoleFleet(fleet, playerName);
-        }
-        return fleet;
-    }
+	private Fleet setupFleet(String playerName) {
+		Fleet fleet = new FleetImpl.Builder().build();
+		int placementMode = setupPlacementMode(playerName);
 
-    private int setupPlacementMode(String playerName) {
-        boolean placementModeChecker = false;
-        int placementMode = -1;
-        while (placementModeChecker == false) {
-            view.showShipPlacementModeView(playerName);
-            if (reader.hasNextInt()) {
-                placementMode = reader.nextInt();
-                if (placementMode == 0 || placementMode == 1) {
-                    placementModeChecker = true;
-                } else {
-                    System.out.printf(ConsoleView.error, "Please select valid Placement mode");
-                }
-            } else {
-                System.out.printf(ConsoleView.error, "Enter valid valid integer value");
-                reader.next();
-            }
-        }
-        return placementMode;
-    }
+		if (placementMode == 1) {
+			fleet = setupRandomShips(fleet);
+		} else if (placementMode == 0) {
+			fleet = setupConsoleFleet(fleet, playerName);
+		}
+		return fleet;
+	}
 
-    private Fleet setupRandomShips(Fleet fleet) {
-        while (!fleet.isAllShipsPlaced()) {
-            try {
-                fleet.placeShipsRandom();
-            } catch (MalformattedException e) {
-                System.out.printf(ConsoleView.error, e.getMessage());
-            }
-        }
-        return fleet;
-    }
+	private int setupPlacementMode(String playerName) {
+		boolean placementModeChecker = false;
+		int placementMode = -1;
+		while (placementModeChecker == false) {
+			view.showShipPlacementModeView(playerName);
+			if (reader.hasNextInt()) {
+				placementMode = reader.nextInt();
+				if (placementMode == 0 || placementMode == 1) {
+					placementModeChecker = true;
+				} else {
+					System.out.printf(ConsoleView.error, "Please select valid Placement mode");
+				}
+			} else {
+				System.out.printf(ConsoleView.error, "Enter valid valid integer value");
+				reader.next();
+			}
+		}
+		return placementMode;
+	}
 
-    private Fleet setupConsoleFleet(Fleet fleet, String playerName) {
-        while (!fleet.isAllShipsPlaced()) {
-            ShipClass shipClass = setupShipToPlace(fleet, playerName);
-            Point startPoint = setupPoint(shipClass);
-            Direction direction = setupDirection(shipClass);
-            try {
-                Ship ship = new ShipImpl.Builder(shipClass).points(startPoint, direction)
-                        .build();
-                fleet.placeShip(ship);
-            } catch (MalformattedException e) {
-                System.out.printf(ConsoleView.error, e.getMessage());
-            } catch (ShipPlacementException e) {
-                System.out.printf(ConsoleView.error, e.getMessage());
-            }
-        }
-        return fleet;
-    }
+	private Fleet setupRandomShips(Fleet fleet) {
+		while (!fleet.isAllShipsPlaced()) {
+			try {
+				fleet.placeShipsRandom();
+			} catch (MalformattedException e) {
+				System.out.printf(ConsoleView.error, e.getMessage());
+			}
+		}
+		return fleet;
+	}
 
-    private ShipClass setupShipToPlace(Fleet fleet, String playerName) {
+	private Fleet setupConsoleFleet(Fleet fleet, String playerName) {
+		while (!fleet.isAllShipsPlaced()) {
+			ShipClass shipClass = setupShipToPlace(fleet, playerName);
+			Point startPoint = setupPoint(shipClass);
+			Direction direction = setupDirection(shipClass);
+			try {
+				Ship ship = new ShipImpl.Builder(shipClass).points(startPoint, direction)
+				        .build();
+				fleet.placeShip(ship);
+			} catch (MalformattedException e) {
+				System.out.printf(ConsoleView.error, e.getMessage());
+			} catch (ShipPlacementException e) {
+				System.out.printf(ConsoleView.error, e.getMessage());
+			}
+		}
+		return fleet;
+	}
 
-        ShipClass shipClass = ShipClass.BARCA;
-        boolean shipToPlaceChecker = false;
-        while (shipToPlaceChecker == false) {
-            view.showFleetShips(fleet);
-            view.showShipsToPlace(playerName, fleet.shipsToPlace());
-            if (reader.hasNextInt()) {
-                int shipIdToPlace = reader.nextInt();
-                if (shipIdToPlace >= 0 && shipIdToPlace < fleet.shipsToPlace()
-                        .size()) {
-                    shipClass = fleet.shipsToPlace()
-                            .get(shipIdToPlace);
-                    shipToPlaceChecker = true;
-                    return shipClass;
-                } else {
-                    System.out.printf(ConsoleView.error, "Please select valid Placement mode");
-                }
-            } else {
-                System.out.printf(ConsoleView.error, "Enter valid valid integer value");
-                reader.next();
-            }
-        }
-        return null;
+	private ShipClass setupShipToPlace(Fleet fleet, String playerName) {
 
-    }
+		ShipClass shipClass = ShipClass.BARCA;
+		boolean shipToPlaceChecker = false;
+		while (shipToPlaceChecker == false) {
+			view.showFleetShips(fleet);
+			view.showShipsToPlace(playerName, fleet.shipsToPlace());
+			if (reader.hasNextInt()) {
+				int shipIdToPlace = reader.nextInt();
+				if (shipIdToPlace >= 0 && shipIdToPlace < fleet.shipsToPlace()
+				        .size()) {
+					shipClass = fleet.shipsToPlace()
+					        .get(shipIdToPlace);
+					shipToPlaceChecker = true;
+					return shipClass;
+				} else {
+					System.out.printf(ConsoleView.error, "Please select valid Placement mode");
+				}
+			} else {
+				System.out.printf(ConsoleView.error, "Enter valid valid integer value");
+				reader.next();
+			}
+		}
+		return null;
 
-    private Point setupPoint(ShipClass shipClass) {
-        boolean pointChecker = false;
-        while (pointChecker == false) {
-            view.showShipPositioningView(shipClass);
-            if (reader.hasNext()) {
-                try {
-                    Point point = new PointImpl.Builder(reader.next()).build();
-                    pointChecker = true;
-                    return point;
-                } catch (MalformattedException e) {
-                    System.out.printf(ConsoleView.error, e.getMessage());
-                    pointChecker = false;
-                }
-            } else {
-                reader.next();
-            }
-        }
-        return null;
-    }
+	}
 
-    private Direction setupDirection(ShipClass shipClass) {
-        boolean directionChecker = false;
-        while (directionChecker == false) {
-            view.showShipDirectionView(shipClass);
-            if (reader.hasNext()) {
-                try {
-                    Direction direction = Direction.getFromShortName(reader.next()
-                            .charAt(0));
-                    directionChecker = true;
-                    return direction;
-                } catch (MalformattedException e) {
-                    System.out.printf(ConsoleView.error, e.getMessage());
-                    directionChecker = false;
-                }
-            } else {
-                reader.next();
-            }
-        }
-        return Direction.DOWN;
-    }
+	private Point setupPoint(ShipClass shipClass) {
+		boolean pointChecker = false;
+		while (pointChecker == false) {
+			view.showShipPositioningView(shipClass);
+			if (reader.hasNext()) {
+				try {
+					Point point = new PointImpl.Builder(reader.next()).build();
+					pointChecker = true;
+					return point;
+				} catch (MalformattedException e) {
+					System.out.printf(ConsoleView.error, e.getMessage());
+					pointChecker = false;
+				}
+			} else {
+				reader.next();
+			}
+		}
+		return null;
+	}
 
-    public boolean isReadyToStart() {
-        return firstPlayer.getFleet()
-                .isAllShipsPlaced()
-                && secondPlayer.getFleet()
-                        .isAllShipsPlaced();
-    }
+	private Direction setupDirection(ShipClass shipClass) {
+		boolean directionChecker = false;
+		while (directionChecker == false) {
+			view.showShipDirectionView(shipClass);
+			if (reader.hasNext()) {
+				try {
+					Direction direction = Direction.getFromShortName(reader.next()
+					        .charAt(0));
+					directionChecker = true;
+					return direction;
+				} catch (MalformattedException e) {
+					System.out.printf(ConsoleView.error, e.getMessage());
+					directionChecker = false;
+				}
+			} else {
+				reader.next();
+			}
+		}
+		return Direction.DOWN;
+	}
 
-    private void receiveShot() {
-        try {
-            if (currentPlayer instanceof ConsolePlayer) {
-                view.showShots(currentPlayer.getShots(), currentPlayer.getName());
-                System.out.println(firstPlayer.getShots());
-                firstPlayer.getShots()
-                        .forEach((k, v) -> {
-                            System.out.println("[" + PointDecoder.pointToString(k) + "]-" + v);
-                        });
-            }
-            Point shotPoint = currentPlayer.prepareShot();
-            if (shotPoint.isInsideBoard()) {
-                System.out.println(
-                        "Player " + currentPlayer.getName() + " your shot is " + PointDecoder.pointToString(shotPoint));
-                if (!currentPlayer.isAlreadyShooted(shotPoint)) {
-                    Player oponent = getOponent();
-                    Optional<Ship> optShip = oponent.getFleet()
-                            .shipAt(shotPoint);
-                    optShip.ifPresent(ship -> {
-                        ship.shoot();
-                        if (ship.isSunk()) {
-                            currentPlayer.setShotSunk(ship);
-                            System.out.printf(ConsoleView.cyan, "Yeah, you kill ship " + ship.getShipClass());
-                            receiveShot();
-                        } else {
-                            currentPlayer.setShot(shotPoint, PointStatus.HIT);
-                            System.out.printf(ConsoleView.cyan, "Yeah, you shot ship " + ship.getShipClass());
-                            receiveShot();
-                        }
+	public boolean isReadyToStart() {
+		return firstPlayer.getFleet()
+		        .isAllShipsPlaced()
+		        && secondPlayer.getFleet()
+		                .isAllShipsPlaced();
+	}
 
-                    });
-                    if (!optShip.isPresent()) {
-                        currentPlayer.setShot(shotPoint, PointStatus.MISS);
-                        switchPlayer();
-                    }
+	private void receiveShot() {
+		try {
+			if (currentPlayer instanceof ConsolePlayer) {
+				view.showShots(currentPlayer.getShots(), currentPlayer.getName());
+				System.out.println(firstPlayer.getShots());
+				firstPlayer.getShots()
+				        .forEach((k, v) -> {
+					        System.out.println("[" + PointDecoder.pointToString(k) + "]-" + v);
+				        });
+			}
+			Point shotPoint = currentPlayer.prepareShot();
+			if (shotPoint.isInsideBoard()) {
+				System.out.println(
+				        "Player " + currentPlayer.getName() + " your shot is " + PointDecoder.pointToString(shotPoint));
+				if (!currentPlayer.isAlreadyShooted(shotPoint)) {
+					Player oponent = getOponent();
+					Optional<Ship> optShip = oponent.getFleet()
+					        .shipAt(shotPoint);
+					optShip.ifPresent(ship -> {
+						ship.shoot();
+						if (ship.isSunk()) {
+							currentPlayer.setShotSunk(ship);
+							System.out.printf(ConsoleView.cyan, "Yeah, you kill ship " + ship.getShipClass());
+							receiveShot();
+						} else {
+							currentPlayer.setShot(shotPoint, PointStatus.HIT);
+							System.out.printf(ConsoleView.cyan, "Yeah, you shot ship " + ship.getShipClass());
+							receiveShot();
+						}
 
-//            optShip.ifPresent(ship->{
-//                ship.shoot();
-//                if(ship.isSunk()) {
-//                    currentPlayer.setShotSunk(ship);
-//                    System.out.printf(ConsoleView.cyan, "Yeah, you kill ship " + ship.getShipClass());
-//                }else {
-//                    currentPlayer.setShot(shotPoint, PointStatus.HIT);
-//                    System.out.printf(ConsoleView.cyan, "Yeah, you shot ship " + ship.getShipClass());
-//                }
-//            });    
+					});
+					if (!optShip.isPresent()) {
+						currentPlayer.setShot(shotPoint, PointStatus.MISS);
+						switchPlayer();
+					}
 
-                } else {
-                    System.out.printf(ConsoleView.error, "You already shot this point. Try one more time");
-                    receiveShot();
-                }
-            } else {
-                System.out.printf(ConsoleView.error, "Please select point inside board!");
-                receiveShot();
-            }
-        } catch (MalformattedException e) {
-            System.out.printf(ConsoleView.error, e.getMessage());
-        }
-    }
+				} else {
+					System.out.printf(ConsoleView.error, "You already shot this point. Try one more time");
+					receiveShot();
+				}
+			} else {
+				System.out.printf(ConsoleView.error, "Please select point inside board!");
+				receiveShot();
+			}
+		} catch (MalformattedException e) {
+			System.out.printf(ConsoleView.error, e.getMessage());
+		}
+	}
 
-    private boolean isGameFinished() {
-        return firstPlayer.hasLost() || secondPlayer.hasLost();
-    }
+	private boolean isGameFinished() {
+		return firstPlayer.hasLost() || secondPlayer.hasLost();
+	}
 
-    private Player getOponent() {
-        return currentPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
-    }
+	private Player getOponent() {
+		return currentPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
+	}
 
-    private void switchPlayer() {
-        currentPlayer = currentPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
-    }
+	private void switchPlayer() {
+		currentPlayer = currentPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
+	}
 
 }
--- src/main/java/battleship/point/PointImpl.java
+++ src/main/java/battleship/point/PointImpl.java
@@ -1,5 +1,6 @@
 package battleship.point;
 
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
@@ -7,117 +8,114 @@ import java.util.TreeSet;
 import battleship.exception.MalformattedException;
 
 public final class PointImpl implements Point, Comparable<Point> {
-    private final int row;
-    private final int column;
-    private int boardSize = 10;
+	private static final Comparator<Point> COMPARATOR = Comparator.comparingInt(Point::getRow)
+	        .thenComparing(Point::getColumn);
+	private final int row;
+	private final int column;
+	private int boardSize = 10;
 
-    public static class Builder {
-        private int row;
-        private int column;
+	public static class Builder {
+		private int row;
+		private int column;
 
-        public Builder(int row, int column) {
-            this.row = row;
-            this.column = column;
-        }
+		public Builder(int row, int column) {
+			this.row = row;
+			this.column = column;
+		}
 
-        /**
-         * Build point with random row and column
-         */
-        public Builder() {
-            Random random = new Random();
-            row = random.nextInt(10);
-            column = random.nextInt(10);
-        }
+		/**
+		 * Build point with random row and column
+		 */
+		public Builder() {
+			Random random = new Random();
+			row = random.nextInt(10);
+			column = random.nextInt(10);
+		}
 
-        /**
-         * Build point from pointString
-         *
-         * @param pointString
-         * @throws MalformattedException
-         */
-        public Builder(String pointString) throws MalformattedException {
-            row = PointDecoder.getRow(pointString);
-            column = PointDecoder.getColumn(pointString);
-        }
+		/**
+		 * Build point from pointString
+		 *
+		 * @param pointString
+		 * @throws MalformattedException
+		 */
+		public Builder(String pointString) throws MalformattedException {
+			row = PointDecoder.getRow(pointString);
+			column = PointDecoder.getColumn(pointString);
+		}
 
-        public PointImpl build() {
-            return new PointImpl(this);
-        }
-    }
+		public PointImpl build() {
+			return new PointImpl(this);
+		}
+	}
 
-    private PointImpl(Builder builder) {
-        this.row = builder.row;
-        this.column = builder.column;
-    }
+	private PointImpl(Builder builder) {
+		this.row = builder.row;
+		this.column = builder.column;
+	}
 
-    @Override
-    public int getRow() {
-        return row;
-    }
+	@Override
+	public int getRow() {
+		return row;
+	}
 
-    @Override
-    public int getColumn() {
-        return column;
-    }
+	@Override
+	public int getColumn() {
+		return column;
+	}
 
-    @Override
-    public boolean isNeighbor(Point other) {
-        return (Math.abs(getRow() - other.getRow()) <= 1) && (Math.abs(getColumn() - other.getColumn()) <= 1);
-    }
+	@Override
+	public boolean isNeighbor(Point other) {
+		return (Math.abs(getRow() - other.getRow()) <= 1) && (Math.abs(getColumn() - other.getColumn()) <= 1);
+	}
 
-    @Override
-    public Set<Point> calculateNeighbors() {
-        Set<Point> toReturn = new TreeSet<>();
-        for (int i = -1; i <= 1; i++) {
-            toReturn.add(new PointImpl.Builder(row - 1, column + i).build());
-            toReturn.add(new PointImpl.Builder(row + 1, column + i).build());
-        }
-        toReturn.add(new PointImpl.Builder(row, column - 1).build());
-        toReturn.add(new PointImpl.Builder(row, column + 1).build());
+	@Override
+	public Set<Point> calculateNeighbors() {
+		Set<Point> toReturn = new TreeSet<>();
+		for (int i = -1; i <= 1; i++) {
+			toReturn.add(new PointImpl.Builder(row - 1, column + i).build());
+			toReturn.add(new PointImpl.Builder(row + 1, column + i).build());
+		}
+		toReturn.add(new PointImpl.Builder(row, column - 1).build());
+		toReturn.add(new PointImpl.Builder(row, column + 1).build());
 
-        return toReturn;
-    }
+		return toReturn;
+	}
 
-    @Override
-    public boolean isInsideBoard() {
-        return row >= 0 && row < boardSize && column >= 0 && column < boardSize;
-    }
+	@Override
+	public boolean isInsideBoard() {
+		return row >= 0 && row < boardSize && column >= 0 && column < boardSize;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null)
-            return false;
-        if (!(o instanceof PointImpl)) {
-            return false;
-        }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null)
+			return false;
+		if (!(o instanceof PointImpl)) {
+			return false;
+		}
 
-        return (row == ((PointImpl) o).row) && (column == ((PointImpl) o).column);
-    }
+		return (row == ((PointImpl) o).row) && (column == ((PointImpl) o).column);
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + column;
-        result = prime * result + row;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + column;
+		result = prime * result + row;
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return "[row=" + row + ", column=" + column + "]";
-    }
+	@Override
+	public String toString() {
+		return "[row=" + row + ", column=" + column + "]";
+	}
 
-    @Override
-    public int compareTo(Point other) {
-        return Double.valueOf(distance(this))
-                .compareTo(distance(other));
-    }
-
-    private double distance(Point p) {
-        return (double) Math.sqrt(Math.pow(p.getRow(), 2) + Math.pow(p.getColumn(), 2));
-    }
+	@Override
+	public int compareTo(Point other) {
+		return COMPARATOR.compare(this, other);
+	}
 
 }
